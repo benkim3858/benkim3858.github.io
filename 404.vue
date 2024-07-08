d75c5841910bce773ba4d14dc32d4b5d69ce53dc
Ben Kim <benkim@Benui-MacBookPro.local>
2024-07-09 01:49:39 +0900

    nuxt config 수정

---
+++ 404.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <h1>404 - 페이지를 찾을 수 없습니다</h1>
+      <NuxtLink to="/">홈으로 돌아가기</NuxtLink>
+    </div>
+  </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    baseURL: '/benkim3858.github.io/', // GitHub 저장소 이름으로 설정
+    buildAssetsDir: 'assets', // 정적 자산 디렉토리 설정
   },
-
-  // GitHub Pages에서는 클라이언트 사이드 렌더링 사용
-  ssr: false,
-
-  compatibilityDate: "2024-07-09"
+  ssr: false, // GitHub Pages에서는 클라이언트 사이드 렌더링 사용
+  target: 'static', // 정적 사이트 생성 모드
+  nitro: {
+    preset: 'github-pages',
+    output: {
+      publicDir: '.output/public'
+    },
+    prerender: {
+      crawlLinks: true,
+      routes: ['/404.html'] // 404 페이지 프리렌더링
+    }
+  }
 })
