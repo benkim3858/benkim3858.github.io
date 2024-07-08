2822282875e40bf994c05b46945971842f312f18
Ben Kim <benkim@Benui-MacBookPro.local>
2024-07-09 01:26:24 +0900

    index page 생성

--- app.vue
+++ app.vue
@@ -1,29 +1,5 @@
 <template>
   <div>
-    <h1>Ben Kim의 포트폴리오</h1>
-    <p>웹 개발자 | Nuxt 3 전문가</p>
-    <section>
-      <h2>프로젝트</h2>
-      <ul>
-        <li>프로젝트 1: 설명</li>
-        <li>프로젝트 2: 설명</li>
-      </ul>
-    </section>
-    <section>
-      <h2>기술 스택</h2>
-      <ul>
-        <li>Vue.js</li>
-        <li>Nuxt 3</li>
-        <li>JavaScript</li>
-      </ul>
-    </section>
+    <NuxtPage />
   </div>
 </template>
-
-<script setup>
-// 필요한 경우 여기에 스크립트 추가
-</script>
-
-<style scoped>
-/* 필요한 경우 여기에 스타일 추가 */
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/benkim3858.github.io/' // GitHub 저장소 이름으로 설정
   },
-  ssr: false, // GitHub Pages에서는 클라이언트 사이드 렌더링 사용
+
+  // GitHub Pages에서는 클라이언트 사이드 렌더링 사용
+  ssr: false,
+
+  compatibilityDate: "2024-07-09"
 })
