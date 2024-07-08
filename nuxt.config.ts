3937a16415247bc439927d05e0cd5b2023470d12
Ben Kim <benkim@Benui-MacBookPro.local>
2024-07-09 02:30:54 +0900

    ...

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/benkim3858.github.io/', // GitHub 저장소 이름으로 설정
-    buildAssetsDir: 'assets', // 정적 자산 디렉토리 설정
+    baseURL: '/benkim3858.github.io/',
+    buildAssetsDir: '_nuxt/'
   },
-
-  // GitHub Pages에서는 클라이언트 사이드 렌더링 사용
   ssr: false,
-
   nitro: {
-    preset: 'github-pages',
-    output: {
-      publicDir: '.output/public'
-    },
-    prerender: {
-      crawlLinks: true,
-      routes: ['/404.html'] // 404 페이지 프리렌더링
-    },
-    static: true
+    preset: 'github-pages'
   },
-
   vite: {
     build: {
-      assetsDir: '_nuxt' // 이 줄을 추가하세요
+      assetsDir: '_nuxt'
     }
-  },
-
-  compatibilityDate: '2024-07-09'
+  }
 })
