e79a45f0efbf3232caf8399b4d38b19dee80907c
Ben Kim <benkim@Benui-MacBookPro.local>
2024-07-09 15:12:06 +0900

    config 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,15 @@
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/benkim3858.github.io/'
-  },
   ssr: false,
   nitro: {
     preset: 'github-pages'
-  },
-  router: {
-    base: '/benkim3858.github.io/'
   },
   experimental: {
     payloadExtraction: false
   },
   generate: {
-    fallback: '404.html'
+    fallback: true
+  },
+  app: {
+    buildAssetsDir: '/assets/'
   }
 })
